907534d976949577cfd33a3a5beb0746532afb8c
ujangaja <ujangaja@gmail.com>
2021-12-01 18:46:32 +0700

    #Section 13: E-Commerce API | 241. Basic Express Server

--- starter/app.js
+++ starter/app.js
@@ -1,1 +1,14 @@
-console.log('E-Commerce API');
+// exppress
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    app.listen(port, console.log(`Server is listening on ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
