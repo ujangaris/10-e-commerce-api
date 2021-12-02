1da1395deb28b2ac85e9abfa4957b016662d9d8d
ujangaja <ujangaja@gmail.com>
2021-12-02 20:17:48 +0700

    #Section 13: E-Commerce API | 255. Refactor JWT Setup

--- starter/controllers/authController.js
+++ starter/controllers/authController.js
@@ -1,7 +1,8 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
+const { createJWT } = require('../utils');
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -15,9 +16,11 @@ const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments({})) === 0;
   const role = isFirstAccount ? 'admin' : 'user';
   const user = await User.create({ name, email, password, role });
-
   const tokenUser = { name: user.name, userId: user._id, role: user.role };
-  const token = jwt.sign(tokenUser, 'jwtSecret', { expiresIn: '1d' });
+  const token = createJWT({ payload: tokenUser });
+
+  // const token = jwt.sign(tokenUser, 'jwtSecret', { expiresIn: '1d' });
+
   res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
 };
 const login = async (req, res) => {
