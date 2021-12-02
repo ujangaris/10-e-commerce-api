91bc0d8a8b31451ed6ffedcb8be6b18c5798c27c
ujangaja <ujangaja@gmail.com>
2021-12-03 03:51:15 +0700

    #Section 13: E-Commerce API | 256. Cookies Setup

--- starter/controllers/authController.js
+++ starter/controllers/authController.js
@@ -20,8 +20,13 @@ const register = async (req, res) => {
   const token = createJWT({ payload: tokenUser });
 
   // const token = jwt.sign(tokenUser, 'jwtSecret', { expiresIn: '1d' });
+  const oneDay = 1000 * 60 * 60 * 24;
+  res.cookie('token', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+  });
 
-  res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 const login = async (req, res) => {
   res.send('login user');
