35dc26eb558da1bcc4dbc6c3f998dde8429ea223
ujangaja <ujangaja@gmail.com>
2021-12-06 20:19:25 +0700

    #Section 13: E-Commerce API | 264. User Routes - Postman

--- starter/controllers/userController.js
+++ starter/controllers/userController.js
@@ -2,16 +2,16 @@ const getAllUsers = async (req, res) => {
   res.send('get all users route');
 };
 const getSingleUser = async (req, res) => {
-  res.send('get single user');
+  res.send(req.params);
 };
 const showCurrentUser = async (req, res) => {
   res.send('show current user');
 };
 const updateUser = async (req, res) => {
-  res.send('update user');
+  res.send(req.body);
 };
 const updateUserPassword = async (req, res) => {
-  res.send('update user password');
+  res.send(req.body);
 };
 
 module.exports = {
