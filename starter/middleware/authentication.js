48ab1ac59ae9e4fc40bee4c086285d854bc18914
ujangaja <ujangaja@gmail.com>
2021-12-07 19:57:09 +0700

    #Section 13: E-Commerce API | 267. Authenticate User - Complete

--- starter/middleware/authentication.js
+++ starter/middleware/authentication.js
@@ -1,13 +1,18 @@
-const CustomErr = require('../errors');
+const CustomError = require('../errors');
 const { isTokenValid } = require('../utils');
 
 const authenticateUser = async (req, res, next) => {
   const token = req.signedCookies.token;
 
   if (!token) {
-    console.log('error, no token present');
-  } else {
-    console.log(' token present');
+    throw new CustomError.UnauthenticatedError('Authentication Invalid');
+  }
+  try {
+    const { name, userId, role } = isTokenValid({ token });
+    // console.log(payload);
+    req.user = { name, userId, role };
+  } catch (error) {
+    throw new CustomError.UnauthenticatedError('Authentication Invalid');
   }
   next();
 };
