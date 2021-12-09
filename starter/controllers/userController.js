bc2864195663b7d4a87df608f991789aac6f5988
ujangaja <ujangaja@gmail.com>
2021-12-09 20:25:56 +0700

    #Section 13: E-Commerce API | 273. UpdateUser Controller - user.save()

--- starter/controllers/userController.js
+++ starter/controllers/userController.js
@@ -18,16 +18,19 @@ const getSingleUser = async (req, res) => {
 const showCurrentUser = async (req, res) => {
   res.status(StatusCodes.OK).json({ user: req.user });
 };
+// update user with user.save()
 const updateUser = async (req, res) => {
   const { email, name } = req.body;
   if (!email || !name) {
     throw new CustomError.BadRequestError('Please provide all values');
   }
-  const user = await User.findOneAndUpdate(
-    { _id: req.user.userId },
-    { email, name },
-    { new: true, runValidators: true }
-  );
+  const user = await User.findOne({ _id: req.user.userId });
+
+  user.email = email;
+  user.name = name;
+
+  await user.save();
+
   const tokenUser = createTokenUser(user);
   attachCookiesToResponse({ res, user: tokenUser });
   res.status(StatusCodes.OK).json({ user: tokenUser });
@@ -55,3 +58,19 @@ module.exports = {
   updateUser,
   updateUserPassword,
 };
+
+// update user with findOneAndUpdate
+// const updateUser = async (req, res) => {
+//   const { email, name } = req.body;
+//   if (!email || !name) {
+//     throw new CustomError.BadRequestError('Please provide all values');
+//   }
+//   const user = await User.findOneAndUpdate(
+//     { _id: req.user.userId },
+//     { email, name },
+//     { new: true, runValidators: true }
+//   );
+//   const tokenUser = createTokenUser(user);
+//   attachCookiesToResponse({ res, user: tokenUser });
+//   res.status(StatusCodes.OK).json({ user: tokenUser });
+// };
