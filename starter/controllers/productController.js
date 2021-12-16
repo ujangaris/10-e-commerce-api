0438931c658b1b52171f3a01d42b4a8581cf36b5
ujangaja <ujangaja@gmail.com>
2021-12-16 21:24:57 +0700

    #Section 13: E-Commerce API | 282. Upload Image Controllers Part 2

--- starter/controllers/productController.js
+++ starter/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require('../models/Product');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
+const path = require('path');
 
 const createProduct = async (req, res) => {
   req.body.user = req.user.userId;
@@ -43,7 +44,26 @@ const deleteProduct = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: 'Success! Product removed.' });
 };
 const uploadImage = async (req, res) => {
-  res.send('upload image');
+  // console.log(req.files);
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No File Upload');
+  }
+  const productImage = req.files.image;
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please Upload Image');
+  }
+  const maxSize = 1024 * 1024;
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      'Please Upload Image smaller than 1MB'
+    );
+  }
+  const imagePath = path.join(
+    __dirname,
+    '../public/uploads/' + `${productImage.name}`
+  );
+  await productImage.mv(imagePath);
+  res.status(StatusCodes.OK).json({ image: `/uploads/${productImage.name}` });
 };
 
 module.exports = {
