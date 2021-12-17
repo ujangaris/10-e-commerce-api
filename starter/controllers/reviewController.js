43f44f6109ba4ea74cc309c73398d573974dbd12
ujangaja <ujangaja@gmail.com>
2021-12-17 16:44:21 +0700

    #Section 13: E-Commerce API | 288. Delete Review Controller

--- starter/controllers/reviewController.js
+++ starter/controllers/reviewController.js
@@ -44,7 +44,16 @@ const updateReview = async (req, res) => {
   res.send('update review');
 };
 const deleteReview = async (req, res) => {
-  res.send('delete review');
+  const { id: reviewId } = req.params;
+
+  const review = await Review.findOne({ _id: reviewId });
+
+  if (!review) {
+    throw new CustomError.NotFoundError(`No review with id ${reviewId}`);
+  }
+  checkPermissions(req.user, review.user);
+  await review.remove();
+  res.status(StatusCodes.OK).json({ msg: 'Success! Review removed' });
 };
 
 module.exports = {
