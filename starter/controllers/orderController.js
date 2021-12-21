95cec6c09569fe0d5999add91f9e4938540aa9fc
ujangaja <ujangaja@gmail.com>
2021-12-21 20:32:12 +0700

    #Section 13: E-Commerce API | 302. Create Order - Check Product part 1

--- starter/controllers/orderController.js
+++ starter/controllers/orderController.js
@@ -1,4 +1,20 @@
+const Order = require('../models/Order');
+const Product = require('../models/Product');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+const { checkPermissions } = require('../utils');
+
 const createOrder = async (req, res) => {
+  const { items: cartItems, tax, shippingFee } = req.body;
+
+  if (!cartItems || cartItems.length < 1) {
+    throw new CustomError.BadRequestError('No cart items provided');
+  }
+  if (!tax || !shippingFee) {
+    throw new CustomError.BadRequestError(
+      'Please provide tax and shipping fee'
+    );
+  }
   res.send('create order');
 };
 const getAllOrders = async (req, res) => {
